19f7286c2875f7f19a9fb733b97937b27ad5723f
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-06 17:53:14 +0200

    add go back button to register step 1 screen

--- src/screens/RegisterStep1.js
+++ src/screens/RegisterStep1.js
@@ -1,10 +1,11 @@
-import { Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, Button } from "react-native";
 
-export default function RegisterStep1() {
+export default function RegisterStep1({ navigation }) {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center bg-white">
         <Text>Register: Step 1</Text>
+        <Button title="Atrás" onPress={() => navigation.navigate("Register")} />
       </View>
     </SafeAreaView>
   );
