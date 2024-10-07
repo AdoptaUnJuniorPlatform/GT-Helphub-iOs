737205672ec798f750e79e71a69b7ded1d070785
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-07 11:03:23 +0200

    test navigation flow

--- src/screens/RegisterStep5.js
+++ src/screens/RegisterStep5.js
@@ -1,10 +1,18 @@
-import { Text, View, SafeAreaView } from "react-native";
+import { Text, View, SafeAreaView, Button } from "react-native";
 
-export default function RegisterStep5() {
+export default function RegisterStep5({ navigation }) {
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1 justify-center items-center bg-white">
         <Text>Register: Step 5</Text>
+        <Button
+          title="Atrás"
+          onPress={() => navigation.navigate("RegisterStep4")}
+        />
+        <Button
+          title="Siguiente"
+          onPress={() => navigation.navigate("RegisterStep6")}
+        />
       </View>
     </SafeAreaView>
   );
