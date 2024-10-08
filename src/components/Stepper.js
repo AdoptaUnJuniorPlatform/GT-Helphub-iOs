5ed9f330e715f9f0f70cde86aa2870366212b245
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-08 15:02:00 +0200

    create StepLabel and Stepper components

---
+++ src/components/Stepper.js
@@ -0,0 +1,30 @@
+import { View } from "react-native";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import StepLabel from "./StepLabel";
+
+const Stepper = ({ step = "1" }) => {
+  const progressPercentage = ((step - 1) / 4) * 100;
+
+  const constrainedLeft = Math.min(Math.max(progressPercentage, 4), 96);
+
+  return (
+    <View className="pt-3">
+      <View className="flex-row items-center">
+        <StepLabel title="1" status="active" />
+        <View className="bg-neutral-color-blue-gray-50 h-[3px] flex-1"></View>
+        <StepLabel title="2" status="inactive" />
+      </View>
+      <View
+        className="absolute top-2 w-[32px] h-[32px] rounded-full bg-neutral-color-blue-gray-50 justify-center items-center"
+        style={{
+          left: `${constrainedLeft}%`,
+          transform: [{ translateX: -16 }],
+        }}
+      >
+        <FontAwesome6 name="user-large" size={14} color="#607d8a" />
+      </View>
+    </View>
+  );
+};
+
+export default Stepper;
