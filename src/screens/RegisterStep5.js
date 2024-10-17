5ba2edb5ecf443b8b45348f01718d39468d91bfe
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-17 09:59:10 +0200

    add responsiveness and ui adjustments of register steps screens

--- src/screens/RegisterStep5.js
+++ src/screens/RegisterStep5.js
@@ -5,15 +5,22 @@ import {
   ScrollView,
   Text,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 import CustomButton from "../components/CustomButton";
 import StepHeader from "../components/StepHeader";
 import StepTitle from "../components/StepTitle";
 import CustomChip from "../components/CustomChip";
-import CustomSelector from "../components/CustomSelector";
+import CustomDropdown from "../components/CustomDropdown";
+import { categories } from "../data/data";
+
+const { width } = Dimensions.get("window");
 
 export default function RegisterStep5({ navigation }) {
-  const [categories, setCategories] = useState([
+  const isSmallScreen = width <= 392;
+  const isBigScreen = width >= 430;
+
+  const [categoriesPop, setCategoriesPop] = useState([
     { label: "Idiomas", active: false },
     { label: "Fitness", active: false },
     { label: "Diseño", active: false },
@@ -26,8 +33,10 @@ export default function RegisterStep5({ navigation }) {
     { label: "Cuidado personal", active: false },
   ]);
 
+  const [selectedCategories, setSelectedcategories] = useState([]);
+
   const toggleCategory = (label) => {
-    setCategories((prevCategories) =>
+    setCategoriesPop((prevCategories) =>
       prevCategories.map((category) =>
         category.label === label
           ? { ...category, active: !category.active }
@@ -37,91 +46,113 @@ export default function RegisterStep5({ navigation }) {
   };
 
   const deleteCategory = (label) => {
-    setCategories((prevCategories) =>
+    setCategoriesPop((prevCategories) =>
       prevCategories.filter((category) => category.label !== label),
     );
   };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView className="flex-1 bg-white px-4">
-        <StepHeader
-          step={"5"}
-          label1={"Mis habilidades"}
-          label2={"Que quiero aprender"}
-          status1={"inactive"}
-          status2={"active"}
-        />
+      <View className="flex-1">
+        <ScrollView className="flex-1 bg-white px-4 pb-20">
+          <StepHeader
+            step={"5"}
+            label1={"Mis habilidades"}
+            label2={"Que quiero aprender"}
+            status1={"inactive"}
+            status2={"active"}
+          />
+          <StepTitle
+            title={"Último paso"}
+            subtitle={"Intercambiando habilidades"}
+          />
+          <View className="flex-1">
+            <View className="flex-1">
+              {/* ¿Qué te gustaría...? */}
+              <View className="mt-4">
+                <Text
+                  className={`text-neutral-color-gray-900 font-roboto-medium ${isBigScreen ? "text-[21px] mb-[8px]" : isSmallScreen ? "text-[18px] mb-[5px]" : "text-[20px] mb-[8px]"}`}
+                >
+                  ¿Qué te gustaría aprender?
+                </Text>
 
-        <StepTitle
-          title={"Paso 5"}
-          subtitle={"Último paso\nIntercambiando habilidades"}
-        />
+                <Text className="text-neutral-color-blue-gray-500 leading-6 font-roboto-regular text-[16px]">
+                  Nos gustaría saber qué te gustaría aprender, para que los
+                  demás usuarios puedan ofrecerte su ayuda.
+                </Text>
+              </View>
 
-        <View className="justify-between flex-grow">
-          <View>
-            {/* ¿Qué te gustaría...? */}
-            <View className="mt-4">
-              <Text className="mb-[8px] text-neutral-color-gray-900 font-roboto-medium text-[20px]">
-                ¿Qué te gustaría aprender?
-              </Text>
-
-              <Text className="text-neutral-color-blue-gray-500 leading-6 font-roboto-regular text-[16px]">
-                Nos gustaría saber qué te gustaría aprender, para que los demás
-                usuarios puedan ofrecerte su ayuda.
-              </Text>
-            </View>
-
-            {/* Categorías */}
-            <View className=" mt-4">
-              <Text className="text-neutral-color-gray-900 font-roboto-medium text-[20px]">
-                Categorías populares
-              </Text>
-              <View className="flex flex-wrap flex-row justify-start align-center gap-2 mt-1">
-                {categories.map((category) => (
-                  <TouchableOpacity
-                    key={category.label}
-                    onPress={() => toggleCategory(category.label)}
-                  >
-                    <CustomChip
-                      label={category.label}
-                      status={category.active ? "active" : "inactive"}
-                      color={category.active ? "blue" : "white"}
-                      iconName={category.active ? "close" : null}
-                      showBorder
-                      onIconPress={() => deleteCategory(category.label)}
-                    />
-                  </TouchableOpacity>
-                ))}
+              {/* Categorías pop */}
+              <View className={`${isSmallScreen ? "mt-2" : "mt-4"}`}>
+                <Text
+                  className={`text-neutral-color-gray-900 font-roboto-medium ${isBigScreen
+                      ? "text-[21px]"
+                      : isSmallScreen
+                        ? "text-[18px]"
+                        : "text-[20px]"
+                    }`}
+                >
+                  Categorías populares
+                </Text>
+                <View
+                  className={`flex flex-wrap flex-row justify-start align-center ${isSmallScreen ? "gap-1" : "gap-2"} mt-1`}
+                >
+                  {categoriesPop.map((category) => (
+                    <TouchableOpacity
+                      key={category.label}
+                      onPress={() => toggleCategory(category.label)}
+                    >
+                      <CustomChip
+                        label={category.label}
+                        status={category.active ? "active" : "inactive"}
+                        color={category.active ? "blue" : "white"}
+                        iconName={category.active ? "close" : null}
+                        showBorder
+                        onIconPress={() => deleteCategory(category.label)}
+                      />
+                    </TouchableOpacity>
+                  ))}
+                </View>
               </View>
 
               {/* Busca */}
-              <View className="mt-4">
-                <Text className="text-neutral-color-gray-900 font-roboto-medium text-[20px] mb-[8px]">
-                  Busca habilidades
+              <View className={`${isSmallScreen ? "mt-3" : "mt-4"} flex-grow`}>
+                <Text
+                  className={`text-neutral-color-gray-900 font-roboto-medium ${isBigScreen
+                      ? "text-[21px]"
+                      : isSmallScreen
+                        ? "text-[18px]"
+                        : "text-[20px]"
+                    } mb-[8px]`}
+                >
+                  Busca más categorías
                 </Text>
-                <CustomSelector />
+                <View className="mb-[60px]">
+                  <CustomDropdown label="Catégories" items={categories} />
+                </View>
               </View>
             </View>
-
-            {/* Navigation Button Set */}
-            <View className="flex-row items-center justify-between mt-12">
-              <CustomButton
-                title="Atrás"
-                onPress={() => navigation.goBack()}
-                width="content"
-                isBackButton
-              />
-              <CustomButton
-                title="Siguiente"
-                onPress={() => navigation.navigate("SessionStart")}
-                variant="white"
-                width="content"
-              />
-            </View>
           </View>
+        </ScrollView>
+
+        {/* Navigation Button Set */}
+        <View
+          className={`absolute ${isSmallScreen ? "pb-2" : ""} bottom-0 left-0 right-0 px-4 pt-2 bg-white flex-row items-center justify-between`}
+        >
+          <CustomButton
+            title="Atrás"
+            onPress={() => navigation.goBack()}
+            width="content"
+            isBackButton
+          />
+          <CustomButton
+            title="Siguiente"
+            onPress={() => navigation.navigate("HomeTabs")}
+            variant="white"
+            width="content"
+          />
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
