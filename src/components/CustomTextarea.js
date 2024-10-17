98866e37e242b674089c9826b7eeb445415371b7
Ekaterina Kushnir <kushnir.ekaterina@gmail.com>
2024-10-17 09:57:02 +0200

    add responsiveness and refactoring of components used in register steps screens

--- src/components/CustomTextarea.js
+++ src/components/CustomTextarea.js
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import { View, Text, TextInput } from "react-native";
 
-const CustomTextarea = ({ value, onChange, placeholder }) => {
+const CustomTextarea = ({
+  value,
+  onChange,
+  placeholder,
+  multiline,
+  numberOfLines,
+  maxLength,
+  height,
+}) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
@@ -10,18 +18,19 @@ const CustomTextarea = ({ value, onChange, placeholder }) => {
         value={value}
         onChangeText={onChange}
         placeholder={placeholder}
-        className={`border-[1px] ${isFocused ? "border-[#212121]" : "border-neutral-color-blue-gray-100"} rounded-[8px] font-roboto-regular h-[146px] bg-transparent text-[14px] ${isFocused ? "text-neutral-color-gray-900" : "text-[#90A4AE]"} p-3`}
+        className={`h-[${height}px] border-[1px] ${isFocused ? "border-[#212121]" : "border-neutral-color-blue-gray-100"} rounded-[8px] font-roboto-regular bg-transparent text-[14px] ${isFocused ? "text-neutral-color-gray-900" : "text-[#90A4AE]"} p-3 pb-6`}
         placeholderTextColor={isFocused ? "#212121" : "#90a3ae"}
-        multiline={true}
-        numberOfLines={7}
-        maxLength={255}
+        multiline={multiline}
+        numberOfLines={numberOfLines}
+        maxLength={maxLength}
+        height={height}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       />
       <Text
         className={`absolute bottom-2 right-3 text-[12px] ${isFocused ? "text-neutral-color-gray-900" : "text-neutral-color-blue-gray-300"}`}
       >
-        {value.length}/255
+        {value.length}/{maxLength}
       </Text>
     </View>
   );
